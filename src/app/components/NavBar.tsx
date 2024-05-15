4a7c84fdbaa832d9d55e25c22464e5b09b0f7b06
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-05-15 12:50:24 +0200

    Update NavBar component to include a purple highlight for the word "Book"

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -5,7 +5,9 @@ const navBar = () => {
     <div className="mx-auto max-w-5xl px6">
       <div className="flex justify-between items-center h-16 w-full">
         <Link href="/">
-          <div className="text-2xl font-bold">Gabs Book</div>
+          <div className="text-2xl font-bold">
+            Gabs<span className=" text-purple-500">Book</span>
+          </div>
         </Link>
       </div>
     </div>
